37a7f1309051394e0337476fb8ada1bd2f903e85
akeamc <ake@amcoff.net>
2024-02-05 15:38:43 +0100

    test

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use pcap::{Capture, Device};
+
+fn main() -> anyhow::Result<()> {
+    let dev = Device::from("wlp2s0");
+    let mut cap = Capture::from_device(dev)?.rfmon(true).promisc(true).open()?;
+
+    while let Ok(packet) = cap.next_packet() {
+        let timeval = packet.header.ts;
+        println!("ts: {}.{}", timeval.tv_sec, timeval.tv_usec);
+    }
+
+    // dbg!(main_device);
+
+    Ok(())
+}
